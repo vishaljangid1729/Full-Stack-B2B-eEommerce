b3615cfc4107b16a8cfb92acd27032144660086d
Vishal Jangid <jangidvishal1999@gmail.com>
2019-10-08 23:39:12 +0530

    Store data from get in touch form

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,72 +1,85 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
+
 var nodemailer = require('nodemailer');
-var home = {
-  name: 'vishal', 
-  age: 32, 
-  address: "svnit"
-}
-mongoose.connect('mongodb+srv://vishal:vishal%401729@cluster0-9qzf0.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 
-var schema = new mongoose.Schema({ name: 'string', size: 'string' });
-var Tank = mongoose.model('Tank', schema);
+const sqlite = require('sqlite3');
 
-var small = new Tank({ size: 'small' });
-
-small.save(function (err) {
-  if (err) return handleError(err);
-  // saved!
-  console.log("data saved");
+const db = new sqlite.Database('./db/database.db', (err)=>{
+  if(err){
+    console.log(err.message);
+  }
+  else{
+    console.log("database connected");
+  }
 });
+
+// get in touch form table created
+
+// db.run("CREATE TABLE get_in_touch (full_name text, email text, mobile text, subject text, message text, time text)", (err)=>{
+//   if(err){
+//     console.log(err.message);
+//   }
+//   else{
+//     console.log("Get in touch table created");
+//   }
+// });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(home)
-  next();
 
-});
 router.post('/', (req, res, next) =>{
-    console.log(req.body);
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'jangidvishal1999@gmail.com',
-      pass: 'vishal@1729'
-    }
-  });
-
-  var mailOptions = {
-    from: 'jangidvishal1999@gmail.com',
-    to: ['jangidvishal1999@gmail.com', 'brodude1729@gmail.com'],
-    subject: req.body.subject,
-    text: `Mail from: ${req.body.email}\nMobile: ${req.body.mobile}\nName: ${req.body.full_name} \n${req.body.message}`
-  };
-  var mailoption2 = {
-    from: 'jangidvishal1999@gmail.com',
-    to: req.body.email,
-    subject: 'no reply',
-    text: 'Thanks for contact with abc.'
-  }
-  transporter.sendMail(mailoption2, (error, info) =>{
-    if(error){
-      console.log(error);
-    }
-    else{
-      console.log("Email sent: " + info.response);
-    }
-  });
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-  
-
-    res.status(200).send();
+      console.log(req.body);
+      var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        port: 465,
+        secure: true,
+        auth: {
+          user: 'jangidvishal1999@gmail.com',
+          pass: 'vishal@1729'
+        }
+      });
+    
+      var mailOptions = {
+        from: 'jangidvishal1999@gmail.com',
+        to: ['jangidvishal1999@gmail.com', 'brodude1729@gmail.com'],
+        subject: req.body.subject,
+        text: `Mail from: ${req.body.email}\nMobile: ${req.body.mobile}\nName: ${req.body.full_name} \n${req.body.message}`
+      };
+      var mailoption2 = {
+        from: 'jangidvishal1999@gmail.com',
+        to: req.body.email,
+        subject: 'no reply',
+        text: 'Thanks for contact with abc.'
+      }
+      transporter.sendMail(mailoption2, (error, info) =>{
+        if(error){
+          console.log(error);
+        }
+        else{
+          console.log("Email sent: " + info.response);
+        }
+      });
+    
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+  // Data stored in database for get in touch inforamtion
+  db.run("INSERT INTO get_in_touch(full_name, email, mobile, subject, message, time) VALUES(?, ?, ?, ?,?, ?)",
+    [req.body.full_name, req.body.email, req.body.mobile, req.body.subject, req.body.message, Date()], (err) => {
+      if (err) {
+        console.log(err.message);
+      }
+      else {
+        console.log("data saved in database for contact info");
+      }
+    });
+      
+    
+        res.status(200).send();
 });
 
 module.exports = router;
